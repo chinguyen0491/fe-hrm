d6051d767c328c79725343ae6606f15bb44d3d80
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-03-28 16:07:06 +0700

    add footer

--- src/Pages/Pages3.js
+++ src/Pages/Pages3.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Banner3 from '../Components/Banner/Banner3'
+import Footer from '../Components/Footer/Footer'
+import Loading from '../Components/Loading/Loading'
 
 function Pages3() {
   return <>
-    
+    <Banner3 />
+    <Loading />
+    <Footer />
   </>
 }
 
