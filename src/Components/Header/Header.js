16d00c476326b6f2a0397c1f609e4ab77cc9420e
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-03-28 13:46:12 +0700

    update press Enter

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,8 +8,17 @@ function Header() {
     const handleChange = e => {
         setInput(e.target.value)
     }
-    const handleSearch = () => {
+    const handleSearch = e => {
         setData(input)
+        e.target.closest(`.${styles.input__wrap}`).querySelector(`.${styles.input}`).value = ''
+        e.target.closest(`.${styles.input__wrap}`).querySelector(`.${styles.input}`).focus()
+    }
+    const handleKeyUp = e => {
+        if (e.keyCode === 13) {
+            setData(input)
+            e.target.value = ''
+            e.target.focus()
+        }
     }
     useEffect(() => {
         console.log(data)
@@ -48,17 +57,17 @@ function Header() {
                 <div className={styles.search__wrap}>
                     <div className={styles.search__column}>
                         <div className={styles.input__wrap}>
-                            <input onChange={handleChange} placeholder='Công việc bạn đang tìm kiếm' type="text" className={styles.input} />
+                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Công việc bạn đang tìm kiếm' type="text" className={styles.input} />
                             <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
                         </div>
                         <div className={styles.input__wrap}>
-                            <input onChange={handleChange} placeholder='Hoặc bạn có thể tìm việc theo' type="text" className={styles.input} />
+                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Hoặc bạn có thể tìm việc theo' type="text" className={styles.input} />
                             <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
                         </div>
                     </div>
                     <div className={styles.search__column}>
                         <div className={styles.input__wrap}>
-                            <input onChange={handleChange} placeholder='Tra cứu kết quả tìm kiếm' type="text" className={styles.input} />
+                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Tra cứu kết quả tìm kiếm' type="text" className={styles.input} />
                             <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
                         </div>
                     </div>
