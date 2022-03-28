f1fd7940b4e7df067a5fad97d18f8562e9e8315f
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-03-28 14:13:39 +0700

    Chia pages

---
+++ src/Components/Banner/Banner3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Banner3.module.css'
+
+function Banner3() {
+  return <>
+    <div className={styles.main}>
+      
+    </div>
+  </>
+}
+
+export default Banner3
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,35 +5,32 @@ import styles from './Header.module.css'
 
 function Header() {
     return <>
-        <header className={styles.main}>
-            <div className={styles.grid}>
-                <Link className={styles.logo} to="/">
-                    <img src="images/logo.png" alt="Logo Diligo" />
+        <header className={styles.grid}>
+            <Link className={styles.logo} to="/">
+                <img src="images/logo.png" alt="Logo Diligo" />
+            </Link>
+            <div className={styles.wrap}>
+                <ul className={styles.nav}>
+                    <li className={styles.nav__item}>
+                        <Link to="/" className={styles.nav__link}>
+                            Trang chủ
+                        </Link>
+                    </li>
+                    <li className={styles.nav__item}>
+                        <Link to="/tuyen-dung" className={styles.nav__link}>
+                            Tuyển dụng
+                        </Link>
+                    </li>
+                    <li className={styles.nav__item}>
+                        <Link to="/hoi-dap" className={styles.nav__link}>
+                            Hỏi đáp
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/login" className={styles.login__btn}>
+                    Đăng nhập
                 </Link>
-                <div className={styles.wrap}>
-                    <ul className={styles.nav}>
-                        <li className={styles.nav__item}>
-                            <Link to="/" className={styles.nav__link}>
-                                Trang chủ
-                            </Link>
-                        </li>
-                        <li className={styles.nav__item}>
-                            <Link to="/tuyen-dung" className={styles.nav__link}>
-                                Tuyển dụng
-                            </Link>
-                        </li>
-                        <li className={styles.nav__item}>
-                            <Link to="/hoi-dap" className={styles.nav__link}>
-                                Hỏi đáp
-                            </Link>
-                        </li>
-                    </ul>
-                    <Link to="/login" className={styles.login__btn}>
-                        Đăng nhập
-                    </Link>
-                </div>
             </div>
-            <Banner1 />
         </header>
     </>
 }
