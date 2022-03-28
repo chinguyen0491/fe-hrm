c6d36058b3a3a4d4113d6df9f13b27646d381079
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-03-28 13:57:12 +0700

    Tach banner  pages 1 2 ra file rieng

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import Banner1 from '../Banner/Banner1'
 import styles from './Header.module.css'
 
 function Header() {
-    const [input, setInput] = useState('')
-    const [data, setData] = useState('')
-    const handleChange = e => {
-        setInput(e.target.value)
-    }
-    const handleSearch = e => {
-        setData(input)
-        e.target.closest(`.${styles.input__wrap}`).querySelector(`.${styles.input}`).value = ''
-        e.target.closest(`.${styles.input__wrap}`).querySelector(`.${styles.input}`).focus()
-    }
-    const handleKeyUp = e => {
-        if (e.keyCode === 13) {
-            setData(input)
-            e.target.value = ''
-            e.target.focus()
-        }
-    }
-    useEffect(() => {
-        console.log(data)
-    }, [data])
     return <>
         <header className={styles.main}>
             <div className={styles.grid}>
@@ -52,27 +33,7 @@ function Header() {
                     </Link>
                 </div>
             </div>
-            <div className={styles.banner}>
-                <img src="images/banner__girl.png" alt="Banner IMG" className={styles.banner__girl} />
-                <div className={styles.search__wrap}>
-                    <div className={styles.search__column}>
-                        <div className={styles.input__wrap}>
-                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Công việc bạn đang tìm kiếm' type="text" className={styles.input} />
-                            <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
-                        </div>
-                        <div className={styles.input__wrap}>
-                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Hoặc bạn có thể tìm việc theo' type="text" className={styles.input} />
-                            <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
-                        </div>
-                    </div>
-                    <div className={styles.search__column}>
-                        <div className={styles.input__wrap}>
-                            <input onKeyUp={handleKeyUp} onChange={handleChange} placeholder='Tra cứu kết quả tìm kiếm' type="text" className={styles.input} />
-                            <i onClick={handleSearch} className="fa-solid fa-magnifying-glass"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Banner1 />
         </header>
     </>
 }
